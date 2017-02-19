d5e096ca8cef17524f7a0a21f26b6c184f738c25
Brian Spates <brian.spates@moz.com>
2017-02-18 20:26:36 -0800

    wip

--- index.js
+++ index.js
@@ -1,32 +1,48 @@
 var Producer = require('./lib/producer');
-var p = new Producer('woot', {
-  clientId: 'wooty',
+var Consumer = require('./lib/consumer');
+
+// var p = new Producer('woot', {
+//   clientId: 'wooty',
+//   host: 'broker0',
+//   port: 9092
+// });
+
+var c = new Consumer('woot', {
+  clientId: 'way',
   host: 'broker0',
   port: 9092
 });
 
-p.write('one', (err, res) => {
-  console.log('one done');
-  if(err) throw err;
-  if(res) console.log(res);
+c.on('data', (chunk) => {
+  console.log(JSON.stringify(chunk, null, 2));
 });
 
-p.write('two', (err, res) => {
-  console.log('two done');
-  if(err) throw err;
-  if(res) console.log(res);
+c.on('error', (err) => {
+  throw err
 });
 
-p.write('three', (err, res) => {
-  console.log('three done');
-  if(err) throw err;
-  if(res) console.log(res);
-});
-
-p.end({
-  welp: true
-}, (err, res) => {
-  console.log('four done');
-  if(err) throw err;
-  if(res) console.log(res);
-});
+// p.write('one', (err, res) => {
+//   console.log('one done');
+//   if(err) throw err;
+//   if(res) console.log(res);
+// });
+//
+// p.write('two', (err, res) => {
+//   console.log('two done');
+//   if(err) throw err;
+//   if(res) console.log(res);
+// });
+//
+// p.write('three', (err, res) => {
+//   console.log('three done');
+//   if(err) throw err;
+//   if(res) console.log(res);
+// });
+//
+// p.end({
+//   welp: true
+// }, (err, res) => {
+//   console.log('four done');
+//   if(err) throw err;
+//   if(res) console.log(res);
+// });
